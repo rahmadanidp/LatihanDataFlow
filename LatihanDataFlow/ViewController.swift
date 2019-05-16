1794061e1c15418e6ad86e26a37074b7e157364f
Rahmadani Pratiwi <rahmadanidp@gmail.com>
2019-05-16 10:19:00 +0700

    Initial Commit

---
+++ LatihanDataFlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LatihanDataFlow
+//
+//  Created by Rahmadani Pratiwi on 16/05/19.
+//  Copyright © 2019 Rahmadani Pratiwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
