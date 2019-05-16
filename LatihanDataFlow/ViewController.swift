f2bb44fbb88b53b26509c53a1cae0522c8290a4f
Rahmadani Pratiwi <rahmadanidp@gmail.com>
2019-05-16 11:22:09 +0700

    latihan halaman sign in dan segue

--- LatihanDataFlow/ViewController.swift
+++ LatihanDataFlow/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        if fullNameTextField.text == "" || phoneTextField.text == "" {
+            let alert = UIAlertController(title: "Error", message: "All text field must be filled.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (alertAction) in
+                alert.dismiss(animated: true, completion: nil)
+            }
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: "toConfirmPage", sender: self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fullNameTextField.delegate = self
+        phoneTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == fullNameTextField{
+            phoneTextField.becomeFirstResponder()
+        } else if textField == phoneTextField{
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+}
